38e1dd07591f0996c8fd55990e393915df678d3e
Mustafa-Fares <mustaphafares.dev@gmail.com>
2025-06-06 01:24:43 +0300

    [UPDATE] update swagger docs

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -7,6 +7,25 @@ use App\Models\Ad;
 
 class AdController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     path="/api/ads",
+     *     summary="Get all active ads",
+     *     tags={"Ads"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/AdResource")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Internal Server Error"
+     *     )
+     * )
+     */
     public function __invoke()
     {
         $ads = Ad::with('media')
--- app/Http/Resources/AdResource.php
+++ app/Http/Resources/AdResource.php
@@ -5,11 +5,35 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @OA\Schema(
+ *     schema="AdResource",
+ *     type="object",
+ *     title="Ad Resource",
+ *     description="Ad resource representation",
+ *     @OA\Property(
+ *         property="id",
+ *         type="integer",
+ *         description="The ID of the ad"
+ *     ),
+ *     @OA\Property(
+ *         property="title",
+ *         type="string",
+ *         description="The title of the ad"
+ *     ),
+ *     @OA\Property(
+ *         property="image",
+ *         ref="#/components/schemas/MediaResource",
+ *         description="The image associated with the ad"
+ *     )
+ * )
+ */
 class AdResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
      *
+     * @param  \Illuminate\Http\Request  $request
      * @return array<string, mixed>
      */
     public function toArray(Request $request): array
