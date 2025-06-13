bca459dcccdf0d82a69b6dc5907d95aa51d42c47
Mustafa-Fares <mustaphafares.dev@gmail.com>
2025-06-13 18:57:33 +0300

    [UPDATE] refine 'media' field in SchoolResource to filter media collections for images and videos, enhancing media retrieval efficiency

--- app/Http/Resources/SchoolResource.php
+++ app/Http/Resources/SchoolResource.php
@@ -85,7 +85,11 @@ class SchoolResource extends JsonResource
             'managerDescription' => $this->manager_description,
             'schoolClasses' => SchoolClassResource::collection($this->whenLoaded('schoolClasses')),
             'managerImage' => MediaResource::make($this->getFirstMedia('managers-images')),
-            'media' => $this->when($this->getAllMedia ,MediaResource::collection($this->media->where('collection_name' , 'images')->orWhere('collection_name' , 'videos'))),
+            'media' => $this->when($this->getAllMedia, MediaResource::collection(
+                $this->media->filter(function ($media) {
+                    return in_array($media->collection_name, ['images', 'videos']);
+                })
+            )),
             'servicesImages' => MediaResource::collection($this->whenLoaded('media' , $this->getMedia('services-images'))),
         ];
     }
